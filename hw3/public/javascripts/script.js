7c1480b51ef9bf26ac94d5416444c840f33a0e56
g-marshak <marshak22@up.edu>
2021-02-13 19:11:16 -0800

    added hw3 express app folder

---
+++ hw3/public/javascripts/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    //order form function
+    $(".button").click(function(){
+        //set text for quantity/topping/notes based on values
+        var qtext = "Quantity: " + $("#quantity").val();
+        var ttext = "Topping: " + $("input:radio[name ='topping']:checked").val();
+        var stext = "Special Instructions: " + $("#notes").val();
+
+        //check for keywords, show/hide elements to show order was completed
+        if(stext.includes("vegan") || stext.includes("Vegan")){
+            alert("Cheesecakes contain milk and eggs. Vegan options are not available.");
+        }else{
+            $("#orderText").html(qtext + "<br>" + ttext + "<br>" + stext);
+            $(".filledOrder").show();
+            $(".orderForm").hide();
+        }
+    });
+
+     //dropdown function
+     $("a").click(function(){
+        $("#monthbtn").html($(this).text());
+     });
+});
