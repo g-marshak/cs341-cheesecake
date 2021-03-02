ddf22b62b8dc574972c470ef11708f7209abfd00
g-marshak <marshak22@up.edu>
2021-03-01 23:04:27 -0800

    hw4 updates

--- hw3/routes/orders.js
+++ hw3/routes/orders.js
@@ -2,14 +2,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET orders (json objects) */
+/* GET orders (json objects) 
 router.get('/', function(req, res, next) {
   res.json({"orders":[
       {"topping":"cherry", "quantity":2},
       {"topping":"chocolate", "quantity":3},
       {"topping":"plain", "quantity":4}
   ]});
-});
+});*/
 
 /*post order handler*/
 router.post('/', function(req, res, next) {
